45b23edd0e9ec597e4773e5c05f0cdfc6f0be833
Girardi <girardimatheus27@gmail.com>
2024-06-29 14:30:15 -0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.attachments:
+        attachment = message.attachments[0]
+        if attachment.url.endswith(('.png', '.jpg', '.jpeg', '.gif')):
+            await attachment.save(attachment.filename)
+
+            # Envia a imagem para outro canal
+            channel_id = #id_do_destinatário
+            channel = bot.get_channel(channel_id)
+            await channel.send(f'Imagem de {message.author}:', file=discord.File(attachment.filename))
+
+    await bot.process_commands(message)
+
+bot.run('YOUR_BOT_TOKEN_HERE')
